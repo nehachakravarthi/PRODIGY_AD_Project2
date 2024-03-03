3b0ee09caf97bd9feb87c2ed62ed04809595408e
nehachakravarthi <69851737+nehachakravarthi@users.noreply.github.com>
2024-03-03 13:32:33 +0530

    Create PopUp.java

---
+++ PopUp.java
@@ -0,0 +1,29 @@
+package com.example.secondproj;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class PopUp extends AppCompatDialogFragment
+{
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Attention!");
+        builder.setMessage("No such task is present");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        return builder.create();
+
+    }
+}
